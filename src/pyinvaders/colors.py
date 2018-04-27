69c46a1b3f63be56cab9d4176b0c50e442b7ceda
Fekete Emil <fekete.feketeemil>
2018-04-27 14:25:36 +0200

    [m][Constants] Create enum representation for color constants.

---
+++ src/pyinvaders/colors.py
@@ -0,0 +1,10 @@
+from enum import Enum, unique
+
+
+@unique
+class Colors(Enum):
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
